cfc3361d752cbd46472b5d25b625b2304d231fc6
MeskiMohammed <mohammedmeski200@gmail.com>
2025-04-22 15:28:51 +0100

    added layout

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/layout', fn()=>view('layout.app'));
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
