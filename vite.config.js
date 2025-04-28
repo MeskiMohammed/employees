39dc51e854876788aee1824fdcaa66f937e0ed3a
mohammedmeski200 <mohammedmeski200@gmail.com>
2025-04-28 16:53:40 +0000

    modified:   .gitignore 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
       refresh: true,
     }),
   ],
+  server: {
+    host: '0.0.0.0', // Allows access from any network
+    https: true, // Enable HTTPS
+    hmr: {
+      host: '8000-idx-employees-stage-project-1745856783394.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev', // Set HMR host for hot module reloading
+      protocol: 'wss', // Use secure WebSocket
+    },
+  },
 });
