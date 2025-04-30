5eaed7872f4e11c8a326e9b95cbf81517031bf67
WHISKY <mohammedmeski200@gmail.com>
2025-04-30 15:31:33 +0100

    added enterprise image

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ \App\Models\Enterprise::first()->name ?? 'Enterprise' }}</title>
-
+    <link rel="icon" href="{{asset('storage/' . \App\Models\Enterprise::first()->logo) }}">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,9 +1,8 @@
 <div id="layout-sidebar" class="bg-base-100 border-r border-base-200 flex flex-col h-screen w-64 fixed left-0 top-0">
     <div class="p-4 border-b border-base-200">
         <a class="flex items-center justify-center" href="/" data-discover="true">
+            <img alt="logo-light" class="h-8 mr-2" src="{{asset('storage/' . \App\Models\Enterprise::first()->logo) }}" />
             {{ \App\Models\Enterprise::first()->name ?? 'Enterprise' }}
-            <img alt="logo-dark" class="hidden h-8 dark:inline" src="/images/logo/logo-dark.svg" />
-            <img alt="logo-light" class="h-8 dark:hidden" src="{{ \App\Models\Enterprise::first()->logo }}" />
         </a>
     </div>
 
